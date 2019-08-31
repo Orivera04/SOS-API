3583cb6192ea6c1e7ed7e3583c133ebf094dd83c
X3R0XP3R <riveramoreira04@gmail.com>
2019-08-31 00:50:51 -0600

    Update autenticacion.Controller.js

--- controllers/autenticacion.Controller.js
+++ controllers/autenticacion.Controller.js
@@ -35,7 +35,7 @@ exports.RestablecerCredenciales = async(Request, Response) => {
                                 });
                             };
 
-                            HtmlFile(__dirname + '../../../assets/correo.html', function(Err, Html) {
+                            HtmlFile(__dirname + '../assets/correo.html', function(Err, Html) {
 
                                 var Template = Handlebars.compile(Html);
                                 var Remplazar = {
