aac0f7e9b5980775411aea0df3d08dbc5f196303
X3R0XP3R <riveramoreira04@gmail.com>
2019-09-02 07:11:54 -0600

    Update averia.Dal.js

--- models/averia.Dal.js
+++ models/averia.Dal.js
@@ -1,28 +1,32 @@
 module.exports = {
     
     /* Obtiene un elemento de la colección */
-    GetAll: (async function(BD, Tipo_Filtro) {
+    GetAll: (async function(BD, Tipo_Filtro,USUARIO) {
         return new Promise(function(OK, Error) {
-            let Query;
 
             // Todas las las averias de un usuario 
             if(Tipo_Filtro == 1){
-                Query = 'SELECT A.*,U.NOMBRE_USUARIO,U.APELLIDO_USUARIO,EA.DESCRIPCION_ESTADO  FROM AVERIA A,USUARIO U,ESTADO_AVERIA EA WHERE A.ID_USUARIO = U.ID_USUARIO AND EA.ID_ESTADO = A.ID_ESTADO';
+                BD.query("SELECT A.*,U.NOMBRE_USUARIO,U.APELLIDO_USUARIO,EA.DESCRIPCION_ESTADO  FROM AVERIA A,USUARIO U,ESTADO_AVERIA EA WHERE A.ID_USUARIO = U.USUARIO AND EA.ID_ESTADO = A.ID_ESTADO AND A.ID_USUARIO = ?", [USUARIO],
+                    function(Err, Filas) {
+                        if (Err) {
+                            Error(Err);
+                        } else {
+                            OK(Filas);
+                        }
+                    });
             }
 
             // Todas las averias sin atender o en progreso
-            else if(Tipo_Filtro == 2){
-                Query = 'SELECT A.*,U.NOMBRE_USUARIO,U.APELLIDO_USUARIO,EA.DESCRIPCION_ESTADO FROM AVERIA A,USUARIO U,ESTADO_AVERIA EA WHERE A.ID_USUARIO = U.ID_USUARIO AND EA.ID_ESTADO = A.ID_ESTADO AND A.ID_ESTADO IN (1,2);';
-            }    
-            
-            BD.query(Query, [],
-                function(Err, Filas) {
-                    if (Err) {
-                        Error(Err);
-                    } else {
-                        OK(Filas);
-                    }
-                });
+            else if(Tipo_Filtro == 2){    
+                BD.query("SELECT A.*,U.NOMBRE_USUARIO,U.APELLIDO_USUARIO,EA.DESCRIPCION_ESTADO FROM AVERIA A,USUARIO U,ESTADO_AVERIA EA WHERE A.ID_USUARIO = U.USUARIO AND EA.ID_ESTADO = A.ID_ESTADO AND A.ID_ESTADO IN (1,2);", [],
+                    function(Err, Filas) {
+                        if (Err) {
+                            Error(Err);
+                        } else {
+                            OK(Filas);
+                        }
+                    });
+                }    
         });
     }),
 
