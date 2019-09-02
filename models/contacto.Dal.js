cdccce4e77cf99c1728c5c3bfb901e151facb564
X3R0XP3R <riveramoreira04@gmail.com>
2019-09-02 05:04:05 -0600

    Update contacto.Dal.js

--- models/contacto.Dal.js
+++ models/contacto.Dal.js
@@ -3,7 +3,7 @@ module.exports = {
     /* Añade un nuevo elemento a la colección */
     Post: (async function(BD, Contacto) {
         return new Promise(function(OK, Error) {
-            BD.query("INSERT INTO `sos`.`USUARIO_CONTACTO`(`ID_USUARIO`,`MENSAJE`) VALUES (?,?,NOW())", [Contacto.ID_USUARIO,Contacto.MENSAJE],
+            BD.query("INSERT INTO `sos`.`USUARIO_CONTACTO`(`ID_USUARIO`,`MENSAJE`,`FECHA`) VALUES (?,?,NOW())", [Contacto.ID_USUARIO,Contacto.MENSAJE],
                 function(Err, Resultado) {
                     if (Err) {
                         Error(Err);
