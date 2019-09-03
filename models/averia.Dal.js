872b7cbf194ae0f409425210d190c670cfe40476
X3R0XP3R <riveramoreira04@gmail.com>
2019-09-02 21:09:24 -0600

    Update averia.Dal.js

--- models/averia.Dal.js
+++ models/averia.Dal.js
@@ -6,7 +6,7 @@ module.exports = {
 
             // Todas las las averias de un usuario 
             if(Tipo_Filtro == 1){
-                BD.query("SELECT A.*, DATE_FORMAT(A.FECHA_AVERIA,'%d/%m/%y') AS FORMATEADA,TE.NOMBRE_ESPECIALIDAD,U.NOMBRE_USUARIO,U.APELLIDO_USUARIO,EA.DESCRIPCION_ESTADO,A.ID_TIPO  FROM AVERIA A,USUARIO U,ESTADO_AVERIA EA, TIPO_ESPECIALIDAD TE WHERE A.ID_USUARIO = U.USUARIO AND EA.ID_ESTADO = A.ID_ESTADO AND A.ID_USUARIO = ? AND TE.ID_ESPECIALIDAD = A.ID_TIPO;", [USUARIO],
+                BD.query("SELECT A.*, DATE_FORMAT(A.FECHA_AVERIA,'%d/%m/%y') AS FORMATEADA,TE.NOMBRE_ESPECIALIDAD,U.NOMBRE_USUARIO,U.APELLIDO_USUARIO,EA.DESCRIPCION_ESTADO,A.ID_TIPO , (SELECT concat(NOMBRE_USUARIO," ",APELLIDO_USUARIO) FROM USUARIO WHERE ID_USUARIO = A.ID_MECANICO ) AS MECANICO FROM AVERIA A,USUARIO U,ESTADO_AVERIA EA, TIPO_ESPECIALIDAD TE  WHERE A.ID_USUARIO = U.USUARIO AND EA.ID_ESTADO = A.ID_ESTADO AND A.ID_USUARIO = ? AND TE.ID_ESPECIALIDAD = A.ID_TIPO ORDER BY ID_AVERIA DESC;", [USUARIO],
                     function(Err, Filas) {
                         if (Err) {
                             Error(Err);
@@ -33,7 +33,7 @@ module.exports = {
     /* Obtiene un elemento de la colección */
     GetByID: (async function(BD, ID) {
         return new Promise(function(OK, Error) {
-            BD.query("SELECT A.*,U.NOMBRE_USUARIO,U.APELLIDO_USUARIO,U.TELEFONO_USUARIO ,UC.* ,EA.DESCRIPCION_ESTADO FROM AVERIA A, USUARIO U , USUARIO_CARRO UC, ESTADO_AVERIA EA WHERE A.ID_AVERIA = ? AND U.USUARIO = A.ID_USUARIO AND UC.ID_USUARIO = A.ID_USUARIO AND EA.ID_ESTADO = A.ID_ESTADO;", [ID],
+            BD.query("SELECT A.*,U.NOMBRE_USUARIO,U.APELLIDO_USUARIO,U.TELEFONO_USUARIO ,UC.* ,EA.DESCRIPCION_ESTADO ,(SELECT concat(NOMBRE_USUARIO," ",APELLIDO_USUARIO) from USUARIO where id_usuario = A.ID_MECANICO )   FROM AVERIA A, USUARIO U , USUARIO_CARRO UC, ESTADO_AVERIA EA WHERE A.ID_AVERIA = ? AND U.USUARIO = A.ID_USUARIO AND UC.ID_USUARIO = A.ID_USUARIO AND EA.ID_ESTADO = A.ID_ESTADO", [ID],
                 function(Err, Filas) {
                     if (Err) {
                         Error(Err);
@@ -62,7 +62,7 @@ module.exports = {
     /* Actualiza un nuevo elemento a la colección */
     Put: (async function(BD, Averia) {
         return new Promise(function(OK, Error) {
-            BD.query("UPDATE `sos`.`AVERIA` SET `ID_USUARIO` = ?, `LATITUD_UBICACION` = ?, `LONGITUD_UBICACION` = ?, `CALLE` = ?, `PROVINCIA` = ?, `DEPARTAMENTO` = ?, `DATOS_ADICIONALES` = ?, `FECHA_AVERIA` = ?, `CALIFICACION` = ?, `ID_ESTADO` = ?,`ID_MECANICO` = ? WHERE `ID_AVERIA` = ?", [Averia.ID_USUARIO,Averia.LATITUD_UBICACION,Averia.LONGITUD_UBICACION,Averia.CALLE,Averia.PROVINCIA,Averia.DEPARTAMENTO,Averia.DATOS_ADICIONALES,Averia.FECHA_AVERIA,Averia.CALIFICACION,Averia.ID_ESTADO,Averia.ID_MECANICO,Averia.ID_AVERIA],
+            BD.query("UPDATE `sos`.`AVERIA` SET `ID_MECANICO` = ?, `ID_ESTADO` = ? WHERE `ID_AVERIA` = ?; ",[Averia.ID_MECANICO,Averia.ID_ESTADO,Averia.ID_AVERIA],
                 function(Err, Resultado) {
                     if (Err) {
                         Error(Err);
